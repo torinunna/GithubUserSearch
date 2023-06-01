58ab308c6ee7cc02e372deb0cc3074d3b8ae8288
torinunna <torinunna9588@gmail.com>
2023-06-01 16:11:55 +0900

    embed SearchVC to navigationController, add collectionView to SearchVC and create ResultCell

---
+++ GithubUserSearch/ResultCell.swift
@@ -0,0 +1,14 @@
+//
+//  ResultCell.swift
+//  GithubUserSearch
+//
+//  Created by YUJIN KWON on 2023/06/01.
+//
+
+import UIKit
+
+class ResultCell: UICollectionViewCell {
+    static let identifier = "ResultCell"
+    
+    @IBOutlet weak var user: UIView!
+}
--- GithubUserSearch/SearchViewController.swift
+++ GithubUserSearch/SearchViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
