57ca5a82137cba3144999a6fbf7b84a34509deb2
torinunna <torinunna9588@gmail.com>
2023-06-01 16:36:55 +0900

    add searchViewController to SearchVC

--- GithubUserSearch/SearchViewController.swift
+++ GithubUserSearch/SearchViewController.swift
@@ -13,6 +13,30 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchController()
+    }
+    
+    private func embedSearchController() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "torinunna"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        self.navigationItem.searchController = searchController
     }
 }
 
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text
+        print("search: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("search pressed: \(searchBar.text)")
+    }
+}
