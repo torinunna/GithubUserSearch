3bee43fa4ef3f1b045fa3ca2c9d07b6cc2c0cffd
torinunna <torinunna9588@gmail.com>
2023-06-02 18:13:54 +0900

    refactor code with NetworkService and Resource

--- GithubUserSearch/SearchViewController.swift
+++ GithubUserSearch/SearchViewController.swift
@@ -10,6 +10,8 @@ import Combine
 
 class SearchViewController: UIViewController {
     
+    let network = NetworkService(configuration: .default)
+    
     @Published private(set) var users: [SearchResult] = []
     var subscriptisons = Set<AnyCancellable>()
 
@@ -81,24 +83,15 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         print("search pressed: \(searchBar.text)")
         guard let keyword = searchBar.text, !keyword.isEmpty else { return }
-        let base = "https://api.github.com/"
-        let path = "search/users"
-        let params: [String: String] = ["q": keyword]
-        let header: [String: String] = ["Content-Type": "application/json"]
-        var urlComponents = URLComponents(string: base + path)!
-        let queryItems = params.map { (key: String, value: String) in
-            return URLQueryItem(name: key, value: value)
-        }
-        urlComponents.queryItems = queryItems
         
-        var request = URLRequest(url: urlComponents.url!)
-        header.forEach { (key: String, value: String) in
-            request.addValue(value, forHTTPHeaderField: key)
-        }
+        let resource = Resource<SearchUserResponse>(
+            base: "https://api.github.com/",
+            path: "search/users",
+            params: ["q": keyword],
+            header: ["Content-Type": "application/json"]
+        )
         
-        URLSession.shared.dataTaskPublisher(for: request)
-            .map { $0.data }
-            .decode(type: SearchUserResponse.self, decoder: JSONDecoder())
+        network.load(resource)
             .map { $0.items }
             .replaceError(with: [])
             .receive(on: RunLoop.main)
